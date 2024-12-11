ab162b08f1af00a9a7fa8f8515d9467934d423a0
mentalllka <evaskha2.0@gmail.com>
2024-12-12 00:30:48 +0300

    get_column_types.py

--- get_column_types.py
+++ get_column_types.py
@@ -3,12 +3,12 @@ def get_column_types(self, by_number=True) :
   try:
     types = {}
     for i, col_name in enumerate (self.header):
-        column_values = [row[i] for row in self.datal
+        column_values = [row[i] for row in self.datal]
         if all(v.isdigit() for v in column_values):
            col_type = int
         elif all(self._is_float(v) for v in column_values):
             col_type = float
-        elif all(v.lower() in ["true", "false"] for v in column_values
+        elif all(v.lower() in ["true", "false"] for v in column_values)
              col_type = bool
         else:
             col_type = str
