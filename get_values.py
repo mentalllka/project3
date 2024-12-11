c2845a1c63f2c33a9e7a1735d3233add37f1cab4
mentalllka <evaskha2.0@gmail.com>
2024-12-11 23:15:19 +0300

    get_values.py

--- get_values.py
+++ get_values.py
@@ -3,6 +3,6 @@ def get_values(self, column=0):
         col_index = column if isinstance(column, int) else self.header.index(column)
         return [row[col_index] for row in self.data]
     except ValueError:
-        raise ValueError(f"Column {column} not found.")
+        raise ValueError(f"Столбец {column} не найден.")
     except Exception as e:
-        raise Exception(f"Error in get_values: {e}")
+        raise Exception(f"Ошибка в get_values: {e}")
